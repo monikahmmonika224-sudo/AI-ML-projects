1f74dc577f093818f62f7886fbf793942b4f3cc7
monikahmmonika224-sudo <monikahmmonika224@gmail.com>
2026-08-31 22:31:23 -0700

    Add files via upload

---
+++ exp2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+df=pd.read_csv(r'C:\Users\lenovo\Documents\ai ml lab\mtcars1.csv')
+print(df.head(10))
+print(plt.hist(x=df['mpg']))
+plt.show()
+print(plt.scatter(x='wt',y='mpg',data=df))
+plt.show()
+print(df['am'].value_counts().plot(kind='bar'))
+plt.show()
+print(sns.boxplot(df['mpg']))
+plt.show()
+print(df['mpg'].min())
+print(df['mpg'].max())
+print(df['mpg'].quantile([.1,.25,.5,.75]))
